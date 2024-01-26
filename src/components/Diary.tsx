6a5d62af5ed864b963afcfbb3742ae1be998def0
김예림 <yrgim@marketboro.com>
2024-01-26 15:04:55 +0900

    다이어리 상세페이지 이미지 추가

--- src/components/Diary.tsx
+++ src/components/Diary.tsx
@@ -2,10 +2,35 @@ const Diary = () => {
   // 모달 창으로 구현? 아님 그냥 옆에?
   return (
     <>
-      <label htmlFor="mood">기분</label>
-      <input type="text" placeholder="오늘의 기분은? " id="mood" />
-      <label htmlFor="diary"></label>
-      <input type="text" placeholder="다이어리 " />
+      <div>
+        <label htmlFor="mood">오늘의 기분은 어떤가요? </label>
+        <input type="text" placeholder="오늘의 기분은?" id="mood" />
+        <div className="diary-button-box">
+          <button>😀</button>
+          <button>🥲</button>
+          <button>🥳</button>
+          <button>🤒</button>
+          <button>🤬</button>
+          <button>🤪</button>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="weather">오늘의 날씨는 어떤가요?</label>
+        <input type="text" placeholder="오늘 날씨는?" id="weather" />
+        <div className="diary-button-box">
+          <button>🌞</button>
+          <button>🌦️</button>
+          <button>☁️</button>
+          <button>🌈</button>
+          <button>☔</button>
+          <button>️⛄️</button>
+        </div>
+      </div>
+      <div className="">
+        <label htmlFor="diary">일기</label>
+        <input type="text" placeholder="" id="diary" />
+      </div>
+      <button>등록</button>
     </>
   );
 };
