a0242fb844288f7de554e40bb46a8e29aa3c77dc
yerimgim <imyrgim92@gmail.com>
2024-02-16 00:11:47 +0900

    react-quill 적용

--- src/components/DiaryDialog.tsx
+++ src/components/DiaryDialog.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Diary = () => {
+  const modules = {
+    toolbar: {
+      container: [
+        ["image"],
+        [{ header: [1, 2, 3, 4, 5, false] }],
+        ["bold", "underline"],
+      ],
+    },
+  };
+  const [content, setContent] = useState("");
+  console.log(content);
+  const [title, setTitle] = useState("");
+  const handleTitleChange = (e) => {
+    setTitle(e.currentTarget.value);
+  };
+  const handleSubmit = async () => {
+    const date = new Date();
+    try {
+      console.log("post data");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="">
       <div>
-        <h3>오늘의 기분은 어떤가요?</h3>
+        <h3 className="font-medium">오늘의 기분은 어떤가요?</h3>
         <div className="diary-button-box">
           <button>😀</button>
           <button>🥲</button>
@@ -26,9 +51,14 @@ const Diary = () => {
         </div>
       </div>
       <div className="">
-        <label htmlFor="diary">일기</label>
-        {/* <input type="text" placeholder="" id="diary" /> */}
-        <ReactQuill />
+        <div>
+          <label htmlFor="diary">일기</label>
+          <ReactQuill
+            style={{ width: "400px", height: "300px" }}
+            modules={modules}
+            onChange={setContent}
+          />
+        </div>
       </div>
     </div>
   );
